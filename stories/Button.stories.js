b254769a93b6e1df4c592ade2620b38db0b87e75
Madhura Mhatre <madhuramhatre95@gmail.com>
2026-08-05 09:58:01 +0000

    Render all 19 Figma button variants in Storybook

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,10 +1,10 @@
+import React from 'react';
 import { fn } from 'storybook/test';
+import { Box } from './Button';
 
-import { createButton } from './Button';
-
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
+  component: Box,
   tags: ['autodocs'],
   parameters: {
     design: {
@@ -12,49 +12,7 @@ export default {
       url: 'https://www.figma.com/design/ak0IV3s1bbMRn3lrWQGxyj/Copy-of-Customer-Facing-Design-System--Copy-?node-id=421-4878&m=dev',
     },
   },
-  render: ({ label, ...args }) => {
-    // You can either use a function to create DOM elements or use a plain html string!
-    // return `<div>${label}</div>`;
-    return createButton({ label, ...args });
-  },
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    label: { control: 'text' },
-    onClick: { action: 'onClick' },
-    primary: { control: 'boolean' },
-    size: {
-      control: { type: 'select' },
-      options: ['small', 'medium', 'large'],
-    },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
   args: { onClick: fn() },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+export const AllFigmaVariants = () => <Box />;
