3546e553ea58bf4003bb78a584613a939dee0762
Madhura Mhatre <madhuramhatre95@gmail.com>
2026-08-06 11:41:03 +0000

    Fix Storybook addons array to use installed addon-docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,8 +5,7 @@ const config = {
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-designs"
   ],
