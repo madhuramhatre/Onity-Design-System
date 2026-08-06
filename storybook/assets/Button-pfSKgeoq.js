f82b265c07758e17f482f4de3740d509527d449e
Madhura Mhatre <madhuramhatre95@gmail.com>
2026-08-06 12:02:38 +0000

    Clean old build outputs for Chromatic

---
+++ storybook/assets/Button-pfSKgeoq.js
@@ -0,0 +1,7 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{i as t,r as n}from"./react-hSQlAL9b.js";import{t as r}from"./jsx-runtime-DeHZSEgm.js";import{a as i,o as a,s as o}from"./blocks-CeeqJvsK.js";import{n as s,t as c}from"./Button.stories-DyJu0yC0.js";function l(e){let n={h1:`h1`,h2:`h2`,p:`p`,...t(),...e.components};return(0,d.jsxs)(d.Fragment,{children:[(0,d.jsx)(i,{of:c,title:`Components/Button`}),`
+`,(0,d.jsx)(n.h1,{id:`button`,children:`Button`}),`
+`,(0,d.jsx)(n.p,{children:`The Button component supports primary actions, secondary actions, and a range of visual variants.`}),`
+`,(0,d.jsx)(n.h2,{id:`preview`,children:`Preview`}),`
+`,(0,d.jsx)(a,{id:`components-button--playground`}),`
+`,(0,d.jsx)(n.h2,{id:`all-variants`,children:`All variants`}),`
+`,(0,d.jsx)(a,{id:`components-button--all-variants`})]})}function u(e={}){let{wrapper:n}={...t(),...e.components};return n?(0,d.jsx)(n,{...e,children:(0,d.jsx)(l,{...e})}):l(e)}var d;function f(){return(f=e((()=>{d=r(),n(),o(),s()})))()}f();export{u as default};
