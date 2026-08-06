c2eb8d639dcf3cd20dfaf637f5c1ad91eb865080
Madhura Mhatre <madhuramhatre95@gmail.com>
2026-08-06 11:39:03 +0000

    Configure Storybook viteFinal base path for GitHub Pages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,22 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
-    '@chromatic-com/storybook',
-    '@storybook/addon-vitest',
-    '@storybook/addon-a11y',
-    '@storybook/addon-docs',
-    '@storybook/addon-designs'
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@storybook/addon-designs"
   ],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {}
   },
   async viteFinal(config) {
-    config.base = './';
+    // Ensures assets point correctly to your GitHub Pages subpath
+    config.base = '/Onity-Design-System/';
     return config;
   }
 };
